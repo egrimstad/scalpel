34f3ccb8b6a7baa97aa6409fe5d031e2dc1f7ebd
fredrik00 <fredrik@vatsendvik.com>
2017-09-29 13:35:20 +0200

    Menu structure now makes a bit more sense

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,8 +6,30 @@ import IconButton from 'material-ui/IconButton'
 import Typography from 'material-ui/Typography'
 import MenuIcon from 'material-ui-icons/Menu'
 import MoreMenu from '../MoreMenu/MoreMenu'
+import MoreVert from 'material-ui-icons/MoreVert'
 
 class Header extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			open: false,
+			anchorEl: null
+		}
+
+		this.handleClick = this.handleClick.bind(this)
+		this.handleRequestClose = this.handleRequestClose.bind(this)
+	}
+
+
+	handleClick(event) {
+		this.setState({ open: true, anchorEl: event.currentTarget })
+	}
+
+	handleRequestClose() {
+		this.setState({ open: false })
+	}
+
 	render() {
 		return (
 			<AppBar position="fixed">
@@ -18,9 +40,14 @@ class Header extends Component {
 					<Typography type="title" color="inherit" noWrap>
 						Scalpel
 					</Typography>
-					<IconButton>
-						<MoreMenu />
+					<IconButton
+						aria-owns={this.state.open ? 'more-menu' : null}
+						aria-haspopup="true"
+						onClick={this.handleClick}
+					>
+						<MoreVert />
 					</IconButton>
+					<MoreMenu open={this.state.open} anchorEl={this.state.anchorEl} handleRequestClose={this.handleRequestClose}/>
 				</Toolbar>
 			</AppBar>
 		)
--- src/components/MoreMenu/MoreMenu.js
+++ src/components/MoreMenu/MoreMenu.js
@@ -1,46 +1,23 @@
 import React, { Component } from 'react'
-import MoreVert from 'material-ui-icons/MoreVert'
-import IconButton from 'material-ui/IconButton'
 import Menu, { MenuItem } from 'material-ui/Menu'
 
 class MoreMenu extends Component {
 	constructor(props) {
 		super(props)
-
-		this.state = {
-			anchorEl: null,
-			open: false,
-		}
-	}
-  
-
-	handleClick(event) {
-		this.setState({ open: true, anchorEl: event.currentTarget })
-	}
-
-	handleRequestClose() {
-		this.setState({ open: false })
 	}
 
 	render() {
 		return (
 			<div>
-				<IconButton
-					aria-owns={this.state.open ? 'simple-menu' : null}
-					aria-haspopup="true"
-					onClick={this.handleClick}
-				>
-					<MoreVert />
-				</IconButton>
 				<Menu
-					id="simple-menu"
-					anchorEl={this.state.anchorEl}
-					open={this.state.open}
-					onRequestClose={this.handleRequestClose}
+					id="more-menu"
+					anchorEl={this.props.anchorEl}
+					open={this.props.open}
+					onRequestClose={this.props.handleRequestClose}
 				>
-					<MenuItem onClick={this.handleRequestClose}>Profile</MenuItem>
-					<MenuItem onClick={this.handleRequestClose}>My account</MenuItem>
-					<MenuItem onClick={this.handleRequestClose}>Logout</MenuItem>
+					<MenuItem onClick={this.props.handleRequestClose}>Profile</MenuItem>
+					<MenuItem onClick={this.props.handleRequestClose}>My account</MenuItem>
+					<MenuItem onClick={this.props.handleRequestClose}>Logout</MenuItem>
 				</Menu>
 			</div>
 		)
