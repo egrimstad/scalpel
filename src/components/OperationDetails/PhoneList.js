3a207b9b091592d04f3dd44b38605a9113e46b99
svennhv <svennhv@gmail.com>
2017-10-11 13:34:18 +0200

    Add example phone link

--- src/components/OperationDetails/PhoneList.js
+++ src/components/OperationDetails/PhoneList.js
@@ -1,33 +1,33 @@
 import React, { Component } from 'react'
-import List, { ListItem, ListItemSecondaryAction} from 'material-ui/List';
-import IconButton from 'material-ui/IconButton';
+import List, { ListItem, ListItemSecondaryAction} from 'material-ui/List'
+import IconButton from 'material-ui/IconButton'
 import Phone from 'material-ui-icons/Phone'
 
 class PhoneList extends Component {
 
 	render() {
 		return (<div>
-				<List>
-					<ListItem> Hei
-						<ListItemSecondaryAction>
-							<IconButton aria-label="Call">
-								<Phone />
-							</IconButton>
-						</ListItemSecondaryAction>
-					</ListItem>
-					<ListItem> hhh
-						<ListItemSecondaryAction>
-							<IconButton aria-label="Call">
-								<Phone />
-							</IconButton>
-						</ListItemSecondaryAction></ListItem>
-					<ListItem> fff
-						<ListItemSecondaryAction>
-							<IconButton aria-label="Call">
-								<Phone />
-							</IconButton>
-						</ListItemSecondaryAction></ListItem>
-				</List>
+			<List>
+				<ListItem> Hei
+					<ListItemSecondaryAction>
+						<IconButton aria-label="Call" href="tel:+4712345678">
+							<Phone />
+						</IconButton>
+					</ListItemSecondaryAction>
+				</ListItem>
+				<ListItem> hhh
+					<ListItemSecondaryAction>
+						<IconButton aria-label="Call" href="tel:+4712345678">
+							<Phone />
+						</IconButton>
+					</ListItemSecondaryAction></ListItem>
+				<ListItem> fff
+					<ListItemSecondaryAction>
+						<IconButton aria-label="Call" href="tel:+4712345678">
+							<Phone />
+						</IconButton>
+					</ListItemSecondaryAction></ListItem>
+			</List>
 			  </div>)
 	}
 }
