5f0148e6b11df64b34a91b3b5d9c5ee550ce4cce
svennhv <svennhv@gmail.com>
2017-10-11 13:41:33 +0200

    Add breakpoint variables, as defined in material-ui

--- src/components/OperationDetails/Operation.js
+++ src/components/OperationDetails/Operation.js
@@ -6,7 +6,11 @@ class Operation extends Component {
 	render() {
 		return (<div>
 			<List>
-				<ListItem> Hei</ListItem>
+				<ListItem>
+					<p>Hei aosdifj oiasj dfoijaoiw oiajf oiaj foialiuhaer liufh aliurh lia liah iuha fliuh awliufh liua fliuafliuh aliuh liauhf liuas fliuh awliuh liuawf liuaew fliu awliuefh liauwehfliuahw liuqhw eliuh awe</p>
+					<p> o!!!!!!!!</p>
+
+				</ListItem>
 				<ListItem> hhh </ListItem>
 				<ListItem> fff </ListItem>
 			</List>
