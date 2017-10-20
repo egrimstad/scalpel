62d9f3a4a59ef2115c4aaa613b78ec2502fb25c1
Eivind Grimstad <egrimstad95@gmail.com>
2017-10-20 18:14:36 +0200

    Add bars for actual and planned time

--- src/components/OperationDetails/OperationTimeline.js
+++ src/components/OperationDetails/OperationTimeline.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import * as d3 from 'd3'
+import moment from 'moment'
+
+import { startedPhases, startTime, endTime } from 'utils/operationUtils'
 
 class OperationTimeline extends Component {
 	constructor(props) {
@@ -28,11 +31,47 @@ class OperationTimeline extends Component {
 				.attr('width', '100%')
 				.attr('height', '100%')
 		}
+
+		const operation = this.props.operation
+
+		const width = this.svg.node().getBoundingClientRect().width
+		const height = this.svg.node().getBoundingClientRect().height
+		const start = startTime(operation)
+		const end = endTime(operation)
+
+		const actualHeight = height*0.8
+		const plannedHeight = height - actualHeight
+
+		const time = d3.scaleTime()
+			.domain([moment(start), moment(end)])
+			.range([0, width])
+		
+		this.svg.append('g').selectAll('rect')
+			.data(startedPhases(operation))
+			.enter()
+			.append('rect')
+			.attr('x', phase => time(moment(phase.start)))
+			.attr('height', actualHeight)
+			.attr('width', phase => time(moment(phase.end || end)) - time(moment(phase.start)))
+			.attr('fill', phase => phase.color)
+		
+		this.svg.append('g').selectAll('rect')
+			.data(operation.plannedPhases)
+			.enter()
+			.append('rect')
+			.attr('x', phase => time(moment(phase.start)))
+			.attr('y', actualHeight)
+			.attr('height', plannedHeight)
+			.attr('width', phase => time(moment(phase.end || end)) - time(moment(phase.start)))
+			.attr('fill', phase => phase.color)
+		
+		
+		
 	}
 
 	render() {
 		return (
-			<div
+			<div style={{width: '100%', height: '100%'}}
 				ref = {element => this.container = element}
 			>
 			</div>
--- src/containers/OperationDetails.js
+++ src/containers/OperationDetails.js
@@ -1,10 +1,35 @@
 import { connect } from 'react-redux'
 import OperationDetails from '../components/OperationDetails/OperationDetails'
 
+import moment from 'moment'
+
 
 const mapStateToProps = (state, ownProps) => {
+	const operation = state.operations.find(x => x.id == ownProps.match.params.operationId)
+
+	operation.phases = operation.phases.map(phase => {
+		const opPhase = state.operationPhases.actual.find(opPhase => opPhase.id === phase.id)
+		return {
+			...phase,
+			...opPhase
+		}
+	})
+
+	operation.plannedPhases = operation.plannedPhases.map((phase, i, phases) => {
+		const opPhase = state.operationPhases.planning.find(opPhase => opPhase.id === phase.id)
+		const start = phases.filter((_, j) => j < i).reduce(
+			(acc, current) => acc.clone().add(current.duration, 'minutes'), 
+			moment(phases[0].start))
+		return {
+			...phase,
+			...opPhase,
+			start: start,
+			end: start.clone().add(phase.duration, 'minutes')
+			
+		}
+	})
 	return {
-		operation: state.operations.find(x => x.id == ownProps.match.params.operationId),
+		operation: operation,
 		...ownProps
 	}
 }
