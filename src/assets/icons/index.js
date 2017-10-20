0d2b5e13e21224810d38af53c8dac8acd2801f14
jennysj <je.sj@hotmail.com>
2017-10-20 12:31:32 +0200

    #35 icons...

---
+++ src/assets/icons/index.js
@@ -0,0 +1,11 @@
+import Pencil from './pencil.png'
+import GreenBall from './bullet_ball_green.png'
+import RedBall from './bullet_ball_red.png'
+import YellowBall from './bullet_ball_yellow.png'
+
+export { 
+	Pencil,
+	GreenBall,
+	RedBall,
+	YellowBall
+}
