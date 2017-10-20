d0909e7c3c159c10eec2fd2cca0b207267d63928
Eivind Grimstad <egrimstad95@gmail.com>
2017-10-20 17:52:35 +0200

    remove longpress from timeline

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -33,24 +33,18 @@ class Timeline extends Component {
 		super(props)
 
 		this.container = null
-
-		this.pressTimer = null
-		this.pressTarget = null
-		this.operationID = null
 		this.filter = null
 
 		this.click = this.click.bind(this)
 		this.start = this.start.bind(this)
 		this.cancel = this.cancel.bind(this)
 		this.redirect = this.redirect.bind(this)
-		this.shortPress = this.shortPress.bind(this)
 		this.closeDialog = this.closeDialog.bind(this)
 
 		this.createFilter = this.createFilter.bind(this)
 		this.buildTimeline = this.buildTimeline.bind(this)
 
 		this.state = {
-			phaseDialogOpen: false,
 			operationDrawerOpen: false,
 			selectedOperation: null
 		}
@@ -64,60 +58,34 @@ class Timeline extends Component {
 		this.props.history.push(url)
 	}
 
-	shortPress(operation) {
+	click(operation) {
 		this.setState({
 			selectedOperation: operation,
 			operationDrawerOpen: true
 		})
-	}
 
-	click(operation) {
-		if(this.pressTimer) {
-			clearTimeout(this.pressTimer)
-			this.pressTimer = null
-			this.shortPress(operation)
-		}
-		if(!this.longpress) {
-			d3.select(this.pressTarget)
-				.selectAll('.Timeline-operation-backdrop, .Timeline-planned')
-				.classed('Timeline-operation-click', false)
-			d3.select(this.pressTarget).attr('filter', null)
-			this.pressTimer = null
-		}
+		d3.select(d3.event.currentTarget)
+			.attr('filter', null)		
+			.selectAll('.Timeline-operation-backdrop, .Timeline-planned')
+			.classed('Timeline-operation-click', false)
 
 		return false
 	}
 
-	start(operation) {
-		if(this.pressTimer) return
-		
-		this.pressTarget = d3.event.currentTarget
-		this.operationID = operation.id
-		d3.select(this.pressTarget).attr('filter', 'url(#Timeline-click-filter)')
-		d3.select(this.pressTarget)
+	start() {
+		d3.select(d3.event.currentTarget)
+			.attr('filter', 'url(#Timeline-click-filter)')
 			.selectAll('.Timeline-operation-backdrop, .Timeline-planned')
 			.classed('Timeline-operation-click', true)
-		this.pressTimer = setTimeout(() => {
-			this.longpress = true
-			d3.select(this.pressTarget)
-				.selectAll('.Timeline-operation-backdrop, .Timeline-planned')
-				.classed('Timeline-operation-click', false)
-			d3.select(this.pressTarget).attr('filter', null)
-			this.pressTimer = null
-		}, 1000)
 
 		return false
 	}
 
 	cancel() {
-		if(this.pressTimer) {
-			clearTimeout(this.pressTimer)
-			this.pressTimer = null
-			d3.select(this.pressTarget)
-				.selectAll('.Timeline-operation-backdrop, .Timeline-planned')
-				.classed('Timeline-operation-click', false)
-			d3.select(this.pressTarget).attr('filter', null)
-		}
+		d3.select(d3.event.currentTarget)
+			.attr('filter', null)		
+			.selectAll('.Timeline-operation-backdrop, .Timeline-planned')
+			.classed('Timeline-operation-click', false)
 		return false
 	}
 
