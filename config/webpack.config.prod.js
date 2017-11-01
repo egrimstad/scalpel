41cec449b9c6b204154593bdffcb35b2d48ee92c
Eivind Grimstad <egrimstad95@gmail.com>
2017-11-01 14:43:36 +0100

    Fix bug in build config

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -96,8 +96,8 @@ module.exports = {
 			// Support React Native Web
 			// https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
 			'react-native': 'react-native-web',
-			assets: path.resolve(__dirname, 'src/assets/'),
-			utils: path.resolve(__dirname, 'src/utils/')
+			assets: path.resolve(__dirname, '../src/assets/'),
+			utils: path.resolve(__dirname, '../src/utils/')
 		},
 		plugins: [
 			// Prevents users from importing files from outside of src/ (or node_modules/).
